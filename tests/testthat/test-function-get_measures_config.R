f9a9d6dc754a2f91ff433a76e7ec95f1bf4f3874
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2026-07-31 12:09:23 +0200

    Finish test and add one more

--- tests/testthat/test-function-get_measures_config.R
+++ tests/testthat/test-function-get_measures_config.R
@@ -17,53 +17,17 @@ test_that("get_measures_config() accepts parameters to be overridden", {
   
   parameters[[c("green_roof_ext", "bagrov_value")]] <- 0.75
   result <- f(parameters)
-  result$green_roof[[1]]
-
+  
+  green_roof <- result[["green_roof"]]
+  input_columns <- sapply(green_roof, "[[", "input_column")
+  expect_equal(
+    green_roof[[which(input_columns == "green_roof_ext")]][["bagrov_value"]],
+    0.75
+  )
 })
 
-
-
-parameter_list <- '{
-  "green_roof_ext": {
-    "bagrov_value": 0.65
-  },
-  "green_roof_int": {
-    "bagrov_value": 0.75
-  },
-  "permeable_paving": {},
-  "unpaving": {},
-  "trees_sm": {
-    "tree_volume": 200
-  },
-  "trees_md": {
-    "tree_volume": 300
-  },
-  "trees_lg": {
-    "tree_volume": 400
-  },
-  "to_swale": {
-    "evaporation_factor": 0.1,
-    "overflow_factor": 0.05
-  },
-  "to_surf_infil": {
-    "evaporation_factor": 0.15,
-    "overflow_factor": 0.15
-  },
-  "to_swale_trench": {
-    "evaporation_factor": 0.08,
-    "overflow_factor": 0.1
-  },
-  "to_tree_pit": {
-    "evaporation_factor": 0.2,
-    "overflow_factor": 0.15
-  },
-  "to_trench": {
-    "evaporation_factor": 0.15,
-    "overflow_factor": 0.15
-  },
-  "to_cistern": {
-    "overflow_factor": 0.5
-  }
-}'
-
-kwb.smartwater:::get_measures_config(parameter_list)
+test_that("get_measures_config() accepts an empty parameter list", {
+  f <- kwb.smartwater:::get_measures_config
+  expect_no_error(config <- f(list()))
+  expect_identical(config, f())
+})
